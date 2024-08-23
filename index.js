1ed5f142fe5033b7577224ebe621759fde08555f
treehouseapps <109871372+treehouseapps@users.noreply.github.com>
2024-08-23 19:23:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,15 @@
 const express = require('express');
 const app = express();
-
+const routes = require('./routes/routes')
 const path = require('path');
 
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-
 app.use(express.urlencoded({ extended: true }));
-const routes = require('./routes/routes')
-let word = "yeeeeeeee";
-import route from "./routes/routes.js";
-if(route){word = "route also"}
-else{
-   word = "yee"}
-app.get('/', (req, res) => {
-   res.render('index', { title: word });
-});
+
+
+app.use("/", routes);
 
 app.listen(3000, () => {
     console.log('Server running on port ' + 3000);
