697f080b9097267f4768a035e46f80bb8ba0b206
treehouseapps <109871372+treehouseapps@users.noreply.github.com>
2024-08-23 18:04:10 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,20 +3,33 @@ const app = express();
 const path = require('path');
 
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-
 app.use(express.urlencoded({ extended: true }));
 
-const routes = require('./routes/routes')
+const routes = require('./routes/routes');
+app.use('/', routes);
 
-app.use('/', routes)
-
-/*app.get('/', (req, res) => {
-   res.render('index');
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
 });
-*/
+
+// Catch-all for 404 errors
+app.use((req, res) => {
+    res.status(404).send('Sorry, that route does not exist.');
+});
+
+// Global exception handling
+process.on('uncaughtException', (err) => {
+    console.error('There was an uncaught error', err);
+});
+
+process.on('unhandledRejection', (reason, promise) => {
+    console.error('Unhandled Rejection at:', promise, 'reason:', reason);
+});
+
 app.listen(3000, () => {
     console.log('Server running on port ' + 3000);
 });
