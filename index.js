3b624712077160410c1c5f1486e9033b8e37b8f1
treehouseapps <109871372+treehouseapps@users.noreply.github.com>
2024-08-23 17:49:42 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const routes = require('./routes/routes')
-//require('dotenv').config()
+require('dotenv').config()
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
@@ -11,16 +11,11 @@ app.set('view engine', 'ejs');
 
 app.set('views', path.join(__dirname, '/views'));
 
-try{
-  app.use('/', routes)
-}
-catch(ex){
   app.get('/', (req, res) => {
   try { res.render('index'); }
   catch (er) { res.send(er) }
 });
-console.log(ex)
-}
+
 
 
 app.listen(3000, () => {
