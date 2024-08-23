119b827a00c25fddf52dc3ac41aa3681fae49390
treehouseapps <109871372+treehouseapps@users.noreply.github.com>
2024-08-23 16:45:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
-const express = require('express')
-const app = express()
-const path = require('path')
+const express = require('express');
+const app = express();
+const path = require('path');
 
-app.use(express.static(path.join(__dirname, 'public')))
-//app.set('view engine', 'ejs')
-//app.set('views', path.join(__dirname + '/view')
-app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Uncomment and fix the view engine setup
+// app.set('view engine', 'ejs');
+// app.set('views', path.join(__dirname, 'view'));
+
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-   res.send('<h1 class="title">error == </h1>')
-})
+   res.send('<h1 class="title">error == </h1>');
+});
 
 app.listen(3000, () => {
-    console.log('Server Running in port ' + 3000)
-})
+    console.log('Server running on port ' + 3000);
+});
