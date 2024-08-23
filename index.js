ac8543b74f1dd8fa261e845bcc3a5bc3a481144d
the-tree-house <109871372+treehouseapps@users.noreply.github.com>
2024-08-23 15:47:13 +0300

    change

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+const express = require('express')
+const app = express()
+require('dotenv').config()
+const path = require('path')
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
-});
+app.use(express.static(path.join(__dirname, '/public')))
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/view')
+app.use(express.urlencoded({ extended: true }))
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
+server.get('/', (req, res) => {
+    res.render('index', { title: 'Home is home' })
+})
+
+app.listen(3000, () => {
+    console.log('Server Running in port ' + 3000)
+})
