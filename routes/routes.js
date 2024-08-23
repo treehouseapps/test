e222a9cde94767983d96506284b80b236ff26d4d
treehouseapps <109871372+treehouseapps@users.noreply.github.com>
2024-08-23 18:10:26 +0300

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,18 @@
 const express = require("express")
 const app = express.Router()
 
-const { home, shorten, newpage, remove } = require('../controller/controller')
-app.post('/short', shorten)
-app.get('/', home)
-app.get('/:newLink', newpage)
-app.get('/delete/:id', remove)
+//const { home, shorten, newpage, remove } = require('../controller/controller')
+//app.post('/short', shorten)
+//app.get('/hi', home)
+app.get('/', (req, res)=>{
+   try {
+        const result = await collection.find({})
+        res.render('index', { name: "", result })
+    }
+    catch (ex) { res.send("error is here ==== " + ex) }
+})
+//app.get('/:newLink', newpage)
+//app.get('/delete/:id', remove)
 
 
 module.exports = app
