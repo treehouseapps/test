26694c2ab4698bd4851ed62c19606d7e0cdcd8d4
treehouseapps <109871372+treehouseapps@users.noreply.github.com>
2024-08-23 17:56:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
+// **Importing Required Modules**
 const express = require('express');
 const app = express();
-require('dotenv').config()
 const path = require('path');
 
+// **Setting Up Static Files**
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.urlencoded({ extended: true }));
+
+// **Setting Up View Engine**
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
-app.set('views', path.join(__dirname, '/views'));
+// **Middleware for Parsing URL-Encoded Data**
+app.use(express.urlencoded({ extended: true }));
 
+// **Defining Routes**
 app.get('/', (req, res) => {
-    try { res.render('index'); }
-    catch (er) { res.send("er = "+er) }
+   res.render('index');
 });
 
+// **Starting the Server**
 app.listen(3000, () => {
     console.log('Server running on port ' + 3000);
 });
