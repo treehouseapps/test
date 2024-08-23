b4239c7c881a1f7ef536f8d6180a699866b88e91
treehouseapps <109871372+treehouseapps@users.noreply.github.com>
2024-08-23 17:58:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
-// **Importing Required Modules**
 const express = require('express');
 const app = express();
 const path = require('path');
 
-// **Setting Up Static Files**
 app.use(express.static(path.join(__dirname, 'public')));
 
-// **Setting Up View Engine**
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-// **Middleware for Parsing URL-Encoded Data**
 app.use(express.urlencoded({ extended: true }));
 
-// **Defining Routes**
-app.get('/', (req, res) => {
+const routes = require('./routes/routes')
+
+app.use('/', routes)
+
+/*app.get('/', (req, res) => {
    res.render('index');
 });
-
-// **Starting the Server**
+*/
 app.listen(3000, () => {
     console.log('Server running on port ' + 3000);
 });
