9c73457c080b80923e24be38be5b15363347cb69
treehouseapps <109871372+treehouseapps@users.noreply.github.com>
2024-08-23 16:46:34 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,15 +3,13 @@ const app = express();
 const path = require('path');
 
 app.use(express.static(path.join(__dirname, 'public')));
-
-// Uncomment and fix the view engine setup
-// app.set('view engine', 'ejs');
-// app.set('views', path.join(__dirname, 'view'));
+ app.set('view engine', 'ejs');
+ app.set('views', path.join(__dirname, 'view'));
 
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-   res.send('<h1 class="title">error == </h1>');
+   res.send('index',{title:"yeeeeeeeeee"});
 });
 
 app.listen(3000, () => {
