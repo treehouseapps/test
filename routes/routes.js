3d6f7417ed483b8bd6a1bfa0d745519e77f25e4b
treehouseapps <109871372+treehouseapps@users.noreply.github.com>
2024-08-24 17:47:08 +0300

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -1,12 +1,14 @@
 const express = require("express")
 const app = express.Router()
 const word = "empty"
-try{
-const home = require('../controller/controller')
-if(home){
-  word = "constroller goted ... from router"
+try {
+  const home = require('../controller/controller');
+  if (home) {
+    word = "controller goted ... from router";
   }
-catch(ex){word = "error catched in == "+ ex}
+} catch (ex) {
+  word = "error caught in == " + ex;
+}
 //, shorten, newpage, remove
 //app.post('/short', shorten)
 
