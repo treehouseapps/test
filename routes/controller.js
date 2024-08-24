baf05bf3bcfb4d4105a3ce16932517da7a5f5816
treehouseapps <109871372+treehouseapps@users.noreply.github.com>
2024-08-24 17:53:54 +0300

    Create controller.js

---
+++ routes/controller.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+//const collection = require('../model/model')
+try{
+const home = async (req, res) => {
+    res.render('index',{title: "Collection goted and the error is = "});
+}
+}
+catch (ex){
+
+const home = " helo world in controller"
+}
+    module.exports = home
