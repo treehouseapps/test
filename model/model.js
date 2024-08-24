be578905efc6d33e9dba7b3e5efd0122fae8144e
treehouseapps <109871372+treehouseapps@users.noreply.github.com>
2024-08-24 18:17:25 +0300

    Update model.js

--- model/model.js
+++ model/model.js
@@ -1,28 +1,27 @@
 const express = require('express');
+const app = express();
 const mongoose = require('mongoose');
 
-const app = express();
-
-mongoose.connect('mongodb+srv://Beki:78122775Beki@cluster0.6ypmi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/link')
-    .then(() => {
-        console.log('Database Connected');
-    })
-    .catch(err => {
-        console.error('Database connection error:', err);
-    });
+try {
+    mongoose.connect('mongodb+srv://Beki:78122775Beki@cluster0.6ypmi.mongodb.net/link?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+        .then(() => console.log('Database Connected'))
+        .catch(err => console.log(err));
+} catch (error) {
+    console.log("Error connecting to database:", error);
+}
 
 const schema = new mongoose.Schema({
     link: {
         type: String,
-        required: true // Example of adding validation
+        required: true // Optional: Add validation if needed
     },
     generated: {
         type: String,
-        required: true // Example of adding validation
+        required: true // Optional: Add validation if needed
     },
     newLink: {
         type: String,
-        required: true // Example of adding validation
+        required: true // Optional: Add validation if needed
     },
 });
 
