4002f43e56f8dbfc61e71cec8a522672ef34548b
Bereket Tsegaye <109871372+treehouseapps@users.noreply.github.com>
2025-04-09 21:36:42 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+require('dotenv').config()
 const routes = require('./routes/routes')
 const path = require('path');
 
@@ -11,6 +12,6 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/", routes);
 
-app.listen(3000, () => {
-  console.log('Server running on port ' + 3000);
+app.listen(process.env.PORT, () => {
+  console.log('Server running on port ' + process.env.PORT);
 });
