fe85e4f0348866c3ee3a0230b29cb8d65ebf3f55
Beki Junior <bbekijunior1@gmail.com>
2025-06-03 21:15:35 +0300

    update

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 
-
 app.use("/", routes);
 
 app.listen(process.env.PORT, () => {
